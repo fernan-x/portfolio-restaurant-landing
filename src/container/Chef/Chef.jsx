6d326dd6fea4c6999efd810162f8f524c48c0690
Fernandes Alves Fabien <fabien@code42.fr>
2022-02-09 20:48:05 +0100

    Added : Chef section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,42 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="Chef" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Pellentesque laoreet tellus sed eros varius mollis. Phasellus id
+          euismod elit. Nunc gravida volutpat dolor id molestie. Aliquam ornare
+          est elementum diam porttitor, sit amet semper tellus feugiat. Aliquam
+          eu mattis orci. Praesent ut dui sapien. Vivamus luctus purus nunc, vel
+          volutpat dolor bibendum ut. Nulla sodales nulla laoreet convallis
+          vulputate.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
